7d4f73e06d6a980f414d3536cf7b149aa074d242
Erik Gabriel De lima Rodrigues <106687673+ErikGabriel3110@users.noreply.github.com>
2023-10-09 12:16:41 -0300

    09/10/2023

---
+++ app/ReconhecimentoDeVoz.js
@@ -0,0 +1,11 @@
+ window.SpeechRecognition = window.SpeechRecognitionAlternative || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang= 'pt-br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    console.log(e.results[0][0].transcript)
+}
